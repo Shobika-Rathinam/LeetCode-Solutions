035aa590bb8c1506107f0474b8b2e66a110e235b
Shobika-Rathinam <shobikar2501@gmail.com>
2025-02-09 07:32:56 +0530

    Create HashTableApproach.java

---
+++ 2sum_problem/HashTableApproach.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> numMap = new HashMap<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            System.out.println(complement);
+            System.out.println(nums[i]);
+            if (numMap.containsKey(complement)) {
+                return new int[]{numMap.get(complement), i};
+            }
+            numMap.put(nums[i], i);
+        }
+
+        return new int[]{}; // No solution found
+    }
+}
